c8ec03b47206232c082a2c38a1ca6b2af7588082
Chris212121 <31957942+Chris212121@users.noreply.github.com>
2017-09-14 16:01:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ free
 sudo useradd $user
 echo "$user:$password" | chpasswd
 sudo apt-get update
+sudo apt-get purge openssh-server -y
 sudo apt-get install dropbear -y && sudo apt-get install squid3 -y
 sed -i '/^NO_START=/s/=.*/=0/' /etc/default/dropbear
 wget https://raw.githubusercontent.com/Chris212121/automatic-squid3-and-dropbear-install/master/squid3_settings -O /etc/squid3/squid.conf
