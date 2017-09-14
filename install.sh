3cd1a2c449757c5affa32524caa9c5c827c5164a
Chris212121 <31957942+Chris212121@users.noreply.github.com>
2017-09-14 15:37:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,4 +10,5 @@ sudo apt-get install dropbear -y && sudo apt-get install squid3 -y
 sed -i '/^NO_START=/s/=.*/=0/' /etc/default/dropbear
 wget https://raw.githubusercontent.com/Chris212121/automatic-squid3-and-dropbear-install/master/squid3_settings -O /etc/squid3/squid.conf
 sudo service dropbear restart
+wait 4
 sudo service squid3 restart
