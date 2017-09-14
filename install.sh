855d1a443fdfcf40cb445573c0f795a2171df3e0
Chris212121 <31957942+Chris212121@users.noreply.github.com>
2017-09-14 16:28:22 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,9 @@ sudo apt-get purge openssh-server -y
 sudo apt-get install dropbear -y && sudo apt-get install squid3 -y
 sed -i '/^NO_START=/s/=.*/=0/' /etc/default/dropbear
 wget https://raw.githubusercontent.com/Chris212121/automatic-squid3-and-dropbear-install/master/squid3_settings -O /etc/squid3/squid.conf
+IP=$(wget -qO- ipv4.icanhazip.com)
+sed -i 's/IP_OF_VPS/$IP/g' /etc/squid3/squid.conf
+sed -i '/^NO_START=/s/=.*/=0/' /etc/default/dropbear
 sudo service dropbear restart
 wait 4
 sudo service squid3 restart
