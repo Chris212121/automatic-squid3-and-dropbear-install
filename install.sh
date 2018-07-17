bf5ed64c69db50100f8df65c935d40a208ccb4bf
Chris212121 <31957942+Chris212121@users.noreply.github.com>
2018-07-17 14:36:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,13 +10,21 @@ sudo apt-get update
 sudo apt-get install dropbear -y && sudo apt-get install squid3 -y
 sed -i '/^NO_START=/s/=.*/=0/' /etc/default/dropbear
 sed -i '/^DROPBEAR_PORT=/s/=.*/=113/' /etc/default/dropbear
-wget https://raw.githubusercontent.com/Chris212121/automatic-squid3-and-dropbear-install/master/squid3_settings -O /etc/squid3/squid.conf
 IP=$(wget -qO- ipv4.icanhazip.com)
-sed -i 's/^IP_OF_VPS/$IP/g' /etc/squid3/squid.conf
+os_version=$(lsb_release -r -s)
 sed -i '/^NO_START=/s/=.*/=0/' /etc/default/dropbear
+if [ "$os_version" == 14.04 ];
+then
+wget https://raw.githubusercontent.com/Chris212121/automatic-squid3-and-dropbear-install/master/squid3_settings -O /etc/squid3/squid.conf
+sed -i 's/^IP_OF_VPS/$IP/g' /etc/squid3/squid.conf
+sudo service squid3 restart
+elif [ "$os_version" == 16.04 ];
+wget https://raw.githubusercontent.com/Chris212121/automatic-squid3-and-dropbear-install/master/squid3_settings -O /etc/squid/squid.conf
+sed -i 's/^IP_OF_VPS/$IP/g' /etc/squid/squid.conf
+sudo systemctl restart squid.service
+fi
 sudo service dropbear restart
 wait 4
-sudo service squid3 restart
 wget https://raw.githubusercontent.com/mauricionet/BadVPN-Setup-0.9/master/badvpnsetup.sh
 chmod +x badvpnsetup.sh
 ./badvpnsetup.sh
